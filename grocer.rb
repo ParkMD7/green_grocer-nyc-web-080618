fa330f66736dc1da8af0eca591c478e4b2ea6668
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-08-05 21:20:22 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -47,6 +47,21 @@ def apply_clearance(cart)
   cart
 end
 
+
+
+
 def checkout(cart, coupons)
-  # code here
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
+  final_cart = apply_clearance(couponed_cart)
+  
+  total = 0
+  
+  final_cart.each do |name, properties|
+    binding.pry
+      total += properties[:price] * properties[:count]
+  end
+  
+  total = total * 0.9 if total > 100
+  total
 end
